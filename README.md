79201a680d0d431da7474a5c03a878425fb8a3f5
Hyunji-JANG <linda99624@gmail.com>
2022-08-17 16:51:26 +0900

    js 추가

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Front_end
 
-08-16
-로그인 페이지 이동 완료
-
 ## main page
 
 - index.html
 - main.css
 
+(js) 갤러리 넘어가기 버튼/Pagination 넘어가기 구현 완료
+
+(back) 갤러리 이미지 불러오기
+
 ## best slip awards
 
 - /html/bestAwards.html
 - /css/bestAwards.css
+
+(js) 갤러리 넘어가기 버튼/Pagination 넘어가기 구현 완료
+
+(back) 갤러리 이미지 불러오기
 
 ## about us
 
@@ -24,10 +29,28 @@
 - /html/mySlip.html
 - /css/mySlip.css
 
+(js) 수정 버튼 hover, 클릭 시 수정 페이지로 이동
+
+
+## 다른 유저의 slip page + 댓글/대댓글
+
+- /html/otherUserSlip.html
+- /html/otherUserSlip.css
+
+(js) 팔로우/스크랩/좋아요 버튼 hover, 클릭 구현
+
+(js) 코멘트 버튼 클릭 시 댓글 창 -> 대댓글 창 생성
+
+(back) 코멘트 받아오기
+
+(추가) 댓글 개수 기반 index(pagination), db에서 댓글 불러오기
+
 ## slip page 댓글/대댓글
 
 - /html/slipComments.html
 - /css/slipComments.css
+
+[수정사항] 다른 유저의 slip page로 병합됨(다른 페이지에 사용하기 위한 페이지)
 
 ## 마이페이지
 
@@ -41,15 +64,39 @@
 - /html/myPage2.html
 - /css/myPage2.css
 
+(js) 활동할 이름 입력 후 버튼 클릭하면 변경된 이름 출력
+
+(back) 동일한 이름 존재할 경우 밑에 확인 메시지 띄우기
+
 ### 3. 마이페이지 MBTI 변경화면
 
 - /html/myPage3.html
 - /css/myPage3.css
 
+(js) MBTI 버튼 클릭 시 무지개색 배경으로 변경
+
+(js) 같은 유형끼리 중복 선택 불가
+
+(js) 바꾸기 버튼 클릭 시 페이지 새로고침
+
+(back) 변경된 MBTI 저장하고, 페이지 새로고침될 때 변경된 MBTI 가져와서 출력하기
+
 ### 4. 마이페이지 비밀번호 변경화면
 
 - /html/myPage4.html
 - /css/myPage4.css
+
+(js) 폼 입력한 값 받아오기 완료
+
+[수정중]
+
+(css) 확인 메시지 안보임 처리
+
+(js) 현재 pwd 틀릴 경우 확인 메시지 띄우기
+
+(js) changePWD 조건 걸기
+
+(js) changePWD <> checkPWD인 경우 확인 메시지 띄우기
 
 ## 로그인 페이지
 
@@ -66,5 +113,14 @@
 위의 두 파일은 헤더와 푸터 내용 들어있는 파일
 추후 페이지 추가 시 복붙하기
 
-[최종]
-파일 경로 확인
+# [이미지 파일 경로]
+- /img - 버튼을 제외한 이미지들
+- /img/Button - 버튼 하얀 바탕
+- /img/Button/Button_reverse - 버튼 검은 바탕
+- /img/Button_MBTI - MBTI 버튼 하얀 바탕
+- /img/Button_MBTI_RAINBOW - MBTI 버튼 무지개색 바탕
+- /img/Button/Hash  - 해시태그 검은 바탕
+- /img/Button/Hash_White - 해시태그 하얀 바탕
+
+[추가]
+겹치는 이미지 통일하기
--- js/test.js
+++ js/test.js
@@ -5,16 +5,164 @@ search.addEventListener("click", () => {
   console.log("clicked!!");
 });
 
-function wow() {
-  if (contents != null) {
-    console.log(contents.value + "를 입력했습니다");
+// function wow() {
+//   if (contents != null) {
+//     console.log(contents.value + "를 입력했습니다");
+//   }
+// }
+
+// function arraowClick() {
+//   console.log("arrow Clicked!");
+// }
+
+// function showClickedMBTI() {
+//   console.log("mbti!!");
+// }
+
+// index.html
+// bestAwards.html
+// 위의 두 파일 동일하게 적용
+// 갤러리 넘기는 버튼 눌렀을 때 갤러리 다음 페이지로 넘어가는 기능
+const btnForwardEl = document.querySelector('.btnForward')
+const btnNextdEl = document.querySelector('.btnNext')
+
+const mainPaginationEl1 = document.querySelector('.gallery-page-1 img')
+const mainPaginationEl2 = document.querySelector('.gallery-page-2 img')
+const mainPaginationEl3 = document.querySelector('.gallery-page-3 img')
+let mainGalleryPageIndex = 0
+
+function btnForwardClicked() {
+  mainGalleryPageIndex -= 1
+
+  if (mainGalleryPageIndex == 0) {
+    btnForwardEl.style.visibility = 'hidden'
+    btnNextdEl.style.visibility = 'visible'
+    
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationActice.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+
+  } else if (mainGalleryPageIndex == 2) {
+    btnForwardEl.style.visibility = 'visible'
+    btnNextdEl.style.visibility = 'hidden'
+    
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationActice.png"
+
+  } else {
+    btnForwardEl.style.visibility = 'visible'
+    btnNextdEl.style.visibility = 'visible'
+
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationActice.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+  }
+}
+function btnNextClicked() {
+  mainGalleryPageIndex += 1
+  
+  if (mainGalleryPageIndex == 0) {
+    btnForwardEl.style.visibility = 'hidden'
+    btnNextdEl.style.visibility = 'visible'
+    
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationActice.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+
+  } else if (mainGalleryPageIndex == 2) {
+    btnForwardEl.style.visibility = 'visible'
+    btnNextdEl.style.visibility = 'hidden'
+    
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationActice.png"
+
+  } else {
+    btnForwardEl.style.visibility = 'visible'
+    btnNextdEl.style.visibility = 'visible'
+
+    mainPaginationEl1.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
+    mainPaginationEl2.src = "../img/Button/Btn_GalleryPaginationActice.png"
+    mainPaginationEl3.src = "../img/Button/Btn_GalleryPaginationDisabled.png"
   }
 }
 
-function arraowClick() {
-  console.log("arrow Clicked!");
+// mySlip.html
+
+// otherUserSlip.html
+// function btnClicked(btnName) {
+//   document.querySelector(`#btn${btnName} img`).src = `../img/Button/Button_reverse/Btn_${btnName}_Rev.png`
+// }
+const btnFollowEl = document.querySelector('#btnFollow img')
+function btnFollowClicked() {
+  console.log('clicked!!')
+  btnFollowEl.src = "../img/Button/Button_reverse/Btn_Follow_Rev.png"
 }
 
-function showClickedMBTI() {
-  console.log("mbti!!");
+const btnScrapEl = document.querySelector('#btnScrap img')
+function btnScrapClicked() {
+  console.log('clicked!!')
+  btnScrapEl.src = "../img/Button/Button_reverse/Btn_Scrap_Rev.png"
 }
+
+const btnLikesEl = document.querySelector('#btnLikes img')
+function btnCommentsClicked() {
+  console.log('clicked!!')
+  btnLikesEl.src = "../img/Button/Button_reverse/Btn_Likes_Rev.png"
+}
+
+const btnCommentsEl = document.querySelector('#btnComments img')
+function btnCommentsCliked() {
+  console.log('cliked!!')
+  btnCommentsEl.src = "../img/Button/Btn_CommentsSmall.png"
+  document.querySelector('.slipComments').style.display = 'block'
+  document.querySelector('.nestedReply').style.display = 'none'
+  document.querySelector('.createComments').style.display = 'block'
+}
+
+const btnNestedCommentsEl = document.querySelector('#btnNestedComments img')
+function btnNestedCommentsClicked() {
+  console.log('cliked!!')
+  document.querySelector('.nestedReply').style.display = 'block'
+}
+
+// myPage2.html
+function btnChangeName() {
+  let newUserName = document.getElementById('mypageNewId').value
+  console.log(newUserName)
+  document.querySelector('.currentUserName').innerHTML = newUserName
+}
+
+// myPage3.html
+let newMBTI = []
+function clickedMBTI(MBTI, placeNum) {
+  if (newMBTI[placeNum] == null) {
+    newMBTI[placeNum] = MBTI
+    console.log(`.mbti-${MBTI} img`)
+    document.querySelector(`.mbti-${MBTI} img`).src = `../img/Button/MBTI_RAINBOW/MBTI_${MBTI}_Rainbow.png`
+  }
+}
+function showNewMBTI() {
+  if (newMBTI.length === 4) {
+    console.log(newMBTI)
+    document.querySelector('.currentMBTI').innerHTML = newMBTI.toString().replace(/,/g,"")
+    window.location.reload()
+  }
+}
+
+// myPage4.html
+function changePWD() {
+  let currentPWD = document.getElementById('mypageCurrentPWD').value
+  console.log(currentPWD)
+  let changePWD = document.getElementById('mypageNewPWD').value
+  console.log(changePWD)
+  let checkPWD = document.getElementById('mypageCheckPWD').value
+  console.log(checkPWD)
+
+  if (changePWD !== checkPWD) {
+    console.log('wrong pwd')
+  }
+  
+}
+
